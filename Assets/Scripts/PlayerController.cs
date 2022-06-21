6678138eb3eebeb867a33f3c83d1497c6248f4cc
Vikrant Kataria <vik7am@gmail.com>
2022-06-21 11:02:12 +0530

    Flip character based on user input
    Added player controller script to flip the character based on user's horizontal input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    float speed;
+    float absSpeed;
+    Vector3 scale;
+    float absScaleX;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        speed = Input.GetAxisRaw("Horizontal");
+        absSpeed = Mathf.Abs(speed);
+        animator.SetFloat("Speed", absSpeed);
+        if (speed == 0)
+            return;
+        scale = transform.localScale;
+        absScaleX = Mathf.Abs(scale.x);
+        scale.x = (speed < 0) ? -1 * absScaleX : absScaleX;
+        /*if (speed < 0)
+            scale.x = -1 * absScaleX;
+        else if (speed > 0)
+            scale.x = absScaleX;*/
+        transform.localScale = scale;
     }
 }
