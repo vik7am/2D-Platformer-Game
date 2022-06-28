530ec97ff3d069488da5f29a934b97631438e105
Vikrant Kataria <vik7am@gmail.com>
2022-06-28 17:10:37 +0530

    respawn after falling
    Added a collider under the platform to trigger respawn when player falls from the platform

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
