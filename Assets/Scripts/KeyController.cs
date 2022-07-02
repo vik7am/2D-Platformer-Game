bf0ee63b1cf70b30843b124a01aa65a3cce9a90c
Vikrant Kataria <vik7am@gmail.com>
2022-07-02 14:06:58 +0530

    Added Spin, move up and fade out animation
    Added spin animation to the coin. Coin moves up while fading when player collects it

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -3,6 +3,41 @@ using UnityEngine;
 public class KeyController : MonoBehaviour
 {
     public int score = 10;
+    public float moveUpSpeed = 1.5f;
+    public float fadeOutTime = 1f;
+    bool isCollected = false;
+    SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (isCollected)
+        {
+            MoveUp();
+            Fade();
+        }
+    }
+
+    void MoveUp()
+    {
+        Vector3 position = transform.position;
+        position.y += moveUpSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void Fade()
+    {
+        Color color = sprite.color;
+        float alpha = sprite.color.a;
+        alpha -= (1 / fadeOutTime) * Time.deltaTime;
+        Mathf.Clamp(alpha, 0, 1);
+        color.a = alpha;
+        sprite.color = color;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -10,7 +45,8 @@ public class KeyController : MonoBehaviour
         if (player != null)
         {
             player.PickUpKey(score);
-            Destroy(gameObject);
+            isCollected = true;
+            Destroy(gameObject, fadeOutTime);
         }
     }
 }
