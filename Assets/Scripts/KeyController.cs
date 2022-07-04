29f62e72ae1b25cdf94ce03125c6b7579b8a75ff
Vikrant Kataria <vik7am@gmail.com>
2022-07-04 14:16:55 +0530

    Added move up animation to moveUpandFade state
    1. Added move up animation to the moveUpAndFade state in the animator and moved components of key into the child object.
    2. Removed the code to move key up.

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -3,43 +3,13 @@ using UnityEngine;
 public class KeyController : MonoBehaviour
 {
     public int score = 10;
-    public float moveUpSpeed = 1.5f;
     public float fadeOutTime = 1f;
-    bool isCollected = false;
-    //SpriteRenderer sprite;
     Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        //sprite = GetComponent<SpriteRenderer>();
     }
-
-    private void Update()
-    {
-        if (isCollected)
-        {
-            MoveUp();
-            //Fade();
-        }
-    }
-
-    void MoveUp()
-    {
-        Vector3 position = transform.position;
-        position.y += moveUpSpeed * Time.deltaTime;
-        transform.position = position;
-    }
-
-    /*void Fade()
-    {
-        Color color = sprite.color;
-        float alpha = sprite.color.a;
-        alpha -= (1 / fadeOutTime) * Time.deltaTime;
-        alpha = Mathf.Clamp(alpha, 0, 1);
-        color.a = alpha;
-        sprite.color = color;
-    }*/
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -48,7 +18,6 @@ public class KeyController : MonoBehaviour
         {
             player.PickUpKey(score);
             animator.SetBool("collected", true);
-            isCollected = true;
             Destroy(gameObject, fadeOutTime);
         }
     }
