2224be529525494ea729a6e20ec453e4b558bcbc
Vikrant Kataria <vik7am@gmail.com>
2022-07-04 18:20:19 +0530

    Added patrolling movement to enemy
    Added script for enemy to patroll between the set waypoints.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Vector3 startPosition;
+    Vector3 endPosition;
+    bool moveForward;
+    int direction;
+    public int speed = 2;
+
+    private void Awake()
+    {
+        startPosition = transform.GetChild(0).transform.position;
+        endPosition = transform.GetChild(1).transform.position;
+    }
+
+    void Start()
+    {
+        moveForward = true;
+        direction = 1;
+    }
+
+    void Update()
+    {
+        CheckPatrollingPath();
+        MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        Vector3 position = transform.position;
+        position.x += speed * direction * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void CheckPatrollingPath()
+    {
+        if (moveForward)
+        {
+            if (transform.position.x >= endPosition.x)
+            {
+                moveForward = false;
+                direction = -1;
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else
+        {
+            if (transform.position.x <= startPosition.x)
+            {
+                moveForward = true;
+                direction = 1;
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+}
