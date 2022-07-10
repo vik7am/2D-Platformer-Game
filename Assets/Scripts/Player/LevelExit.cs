6339b1a4d7b4796be7c3abfd6ef3d26c2ad07804
Vikrant Kataria <vik7am@gmail.com>
2022-07-10 17:48:35 +0530

    Code optimization
    Renamed file names and moved them into folders.

--- Assets/Scripts/Player/LevelExit.cs
+++ Assets/Scripts/Player/LevelExit.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelController : MonoBehaviour
+public class LevelExit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -82,18 +82,11 @@ public class PlayerController : MonoBehaviour
 
     void JumpPlayer()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            if (isGrounded)
-            {
-                playerRigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-                animator.SetBool("Jump", true);
-            }
-    }
-
-    public void playDeathAnimation()
-    {
-        animator.SetBool("Jump", false);
-        animator.SetBool("Death", true);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            animator.SetBool("Jump", true);
+        }
     }
 
     public void PickUpKey(int score)
@@ -114,23 +107,37 @@ public class PlayerController : MonoBehaviour
         else
         {
             healthUI.RemoveHeart(0);
-            gameOver.showGameOverScreen();
-            isAlive = false;
-            //animator.SetBool("Death", true);
+            KillPlayer();
         }
     }
-    
+
+    public void KillPlayer()
+    {
+        isAlive = false;
+        animator.SetBool("Jump", false);
+        animator.SetBool("Death", true);
+        StartCoroutine(PlayDeathAnimation());
+    }
+
+    IEnumerator PlayDeathAnimation()
+    {
+        animator.SetBool("Death", true);
+        yield return new WaitForSeconds(1);
+        gameOver.showGameOverScreen();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        isGrounded = true;
-        animator.SetBool("Jump", false);
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            animator.SetBool("Jump", false);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<EnemyController>() != null)
-            return;
-        isGrounded = false;
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
     }
 }
